b99fb0b3aa6ac582f4ec449bd070991f431f2e35
Paulo Filho <paulofilhobarroso@gmail.com>
2022-03-14 19:34:52 -0300

    Project Structure Without Implementations

---
+++ app/src/main/java/com/androidlearn/example/colorpick/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androidlearn.example.colorpick
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
